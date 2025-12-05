3f3a93a3a6c5e95258be0d4843fe4c61799fc634
kany <kanysharapova@gmail.com>
2025-12-05 19:08:49 +0100

    feat: Update Netlify configuration and clean up main.tsx by removing unused router code

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-// import { createBrowserRouter } from "react-router";
-// import { RouterProvider } from "react-router/dom";
-// import Categories from "./pages/Categories.tsx";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/garden-shop",
-//     element: <App />,
-//   },
-//   {
-//     path: "/garden-shop/categories",
-//     element: <Categories />,
-//   },
-// ]);
-
-// createRoot(document.getElementById("root")!).render(
-//   <RouterProvider router={router} />
-// );
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
